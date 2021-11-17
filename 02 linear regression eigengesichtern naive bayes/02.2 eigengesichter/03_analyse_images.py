52e3c6f7044407b7b72ac33d0e0f4dbb39ff891d
Tobias Schmücker <schmuecker.tobias@gmail.com>
2021-11-17 16:22:37 +0100

    Add face classification

--- 02 linear regression eigengesichtern naive bayes/02.2 eigengesichter/03_analyse_images.py
+++ 02 linear regression eigengesichtern naive bayes/02.2 eigengesichter/03_analyse_images.py
@@ -1,17 +1,12 @@
 import sys
-import pathlib
-import shutil
-from os import listdir
-from datetime import datetime
-from os.path import isfile, join, dirname, realpath, basename, exists
-from skimage.io import imread, imsave
-from skimage.util import crop
-from skimage.transform import resize
-import matplotlib.pyplot as plt
+import math
 import numpy as np
 import pandas as pd
+import matplotlib.pyplot as plt
+from os import listdir
+from skimage.io import imread
 from sklearn import decomposition
-import scipy.stats as stats
+from os.path import join, dirname, realpath
 
 
 def load_images(path: str):
@@ -30,37 +25,25 @@ def load_images(path: str):
             images.append(image)
 
     # Flatten images to pixels
-    images_pixels = images
+    images_pixels = [0 for i in range(len(images))]
     for idx, image in enumerate(images):
         images_pixels[idx] = []
         for row in image:
             for pixel in row:
                 images_pixels[idx].append(pixel)
 
-    return images_pixels
+    return images, images_pixels
+
 
 # c)
 
-
 # Load pixels from images
-images_pixels_t = load_images('data-processed/training/')
-images_pixels_te = load_images('data-processed/test/')
+images_t, images_pixels_t = load_images('data-processed/training/')
+images_te, images_pixels_te = load_images('data-processed/test/')
+print(len(images_pixels_t[0]))
 
 # Design matrix
 design_matrix_t = pd.DataFrame(images_pixels_t)
-
-# PCA
-# U, D, V = pca(design_matrix_t)
-# n = U.shape[0]
-
-# pca_result = pd.DataFrame(D, columns=["SingularValue"])
-# pca_result["EigenValue"] = pca_result["SingularValue"] ** 2
-# pca_result["Varianz"] = pca_result["SingularValue"] / (n-1)
-# pca_result["AnteilVarianz%"] = pca_result["Varianz"] / \
-#     (pca_result["Varianz"].sum()) * 100
-# pca_result["Kumuliert%"] = pca_result["AnteilVarianz%"].cumsum()
-# pca_result["Fehler"] = 100 - pca_result["Kumuliert%"]
-# pca_result["Index"] = pca_result.index
 
 # Plot Eigenvalues
 # pca_result.head(150).plot.scatter(x=6, y=1)
@@ -69,12 +52,12 @@ design_matrix_t = pd.DataFrame(images_pixels_t)
 pca = decomposition.PCA(n_components=150, whiten=True)
 pca.fit(images_pixels_t)
 
-fig = plt.figure(figsize=(20, 6))
-for i in range(12):
-    ax = fig.add_subplot(3, 4, i + 1, xticks=[], yticks=[])
-    ax.imshow(pca.components_[i].reshape(32, 32),
-              cmap=plt.cm.bone)
-plt.show()
+# fig = plt.figure(figsize=(20, 6))
+# for i in range(12):
+#     ax = fig.add_subplot(3, 4, i + 1, xticks=[], yticks=[])
+#     ax.imshow(pca.components_[i].reshape(32, 32),
+#               cmap=plt.cm.bone)
+# plt.show()
 
 # Interpretation:
 # - first 4 pc's are lighting conditions (dark, left, bright, right)
@@ -88,9 +71,29 @@ for col in design_matrix_te.columns:
     design_matrix_te[col] = design_matrix_te[col] - design_matrix_t[col].mean()
 
 # Project images to eigenfaces
-# pca.components_[:7] gives the first 7 eigenfaces (PCs)
-# We have to find out how to limit the number of PCs used in the transformation
-# E.g. by creating a new PCA with pca = decomposition.PCA(n_components=7, whiten=True)
+pca = decomposition.PCA(n_components=7, whiten=True)
+pca.fit(images_pixels_t)
+
 X_train_pca = pca.transform(images_pixels_t)
 X_test_pca = pca.transform(images_pixels_te)
-print(X_train_pca.shape)
+
+distances = []
+min_distances = []
+for test_image in X_test_pca:
+    distance = []
+    for train_image in X_train_pca:
+        d = math.dist(test_image, train_image)
+        distance.append(d)
+    distances.append(distance)
+    min_distances.append(np.argmin(distance))
+
+
+# Display classified images
+
+fig = plt.figure(figsize=(20, 6))
+for idx, test_image in enumerate(images_te):
+    ax = fig.add_subplot(2, 7, idx + 1, xticks=[], yticks=[])
+    ax.imshow(test_image, cmap=plt.cm.bone)
+    ax2 = fig.add_subplot(2, 7, idx + 8, xticks=[], yticks=[])
+    ax2.imshow(images_t[min_distances[idx]], cmap=plt.cm.bone)
+plt.show()
